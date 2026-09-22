80e79d1e591ffb8dba624ddda68cfd946f67712a
Jasper Bungay <jbungay@uoguelph.ca>
2026-09-22 15:03:19 -0400

    Initial Commit

---
+++ Lab_1/TeamBoard.java
@@ -0,0 +1,12 @@
+package Lab_1;
+import java.util.Scanner;
+
+public class TeamBoard {
+    private int MAX_ARRAY_SIZE = 10;
+    public String []posts = new String[MAX_ARRAY_SIZE];
+
+    
+
+    
+}
+
