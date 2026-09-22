bfb79c077aa26c76bbbf7efff3e6aba92f6074e2
Jasper Bungay <jbungay@uoguelph.ca>
2026-09-22 15:27:06 -0400

    added new_post() method

--- Lab_1/TeamBoard.java
+++ Lab_1/TeamBoard.java
@@ -2,11 +2,26 @@ package Lab_1;
 import java.util.Scanner;
 
 public class TeamBoard {
-    private int MAX_ARRAY_SIZE = 10;
-    public String []posts = new String[MAX_ARRAY_SIZE];
-
+    public static int MAX_ARRAY_SIZE = 10;
+    public static String []posts = new String[MAX_ARRAY_SIZE];
+    public static Scanner inScanner = new Scanner(System.in);
+    public static int post_items = 0;
     
+    public static void new_post(){
+        if(post_items == MAX_ARRAY_SIZE){
+            System.out.println("Notice Board is full. Cannot add new notice.");
+        } else {
+            System.out.println("please enter your name");
+            String name = inScanner.nextLine();
+            System.out.println("please enter your notice");
+            String notice = inScanner.nextLine();
+            posts[post_items] = ( name + " post: " + notice);
+        }
+        return;
+    }
 
-    
+    public static void main(String[] args){
+        
+    }
 }
 
